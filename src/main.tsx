f42adb7fe8abe6544742eec1e93f31ab5dd2afe6
RedGfox <zim238kosmit@gmail.com>
2025-06-27 17:35:39 +0300

    renema_file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
-import App from './Components/App/App';
+import App from './components/App/App';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
